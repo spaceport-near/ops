ef02daf1a913d44c7cb252cc069f2fc3bacf19ad
segovchik <segovchik@gmail.com>
2022-10-20 17:54:18 +0200

    Add org members

---
+++ terraform/github/organisation.tf
@@ -0,0 +1,25 @@
+data "github_organization" "spaceport_near" {
+  name = "spaceport-near"
+}
+
+module "organization" {
+  source  = "mineiros-io/organization/github"
+  version = "~> 0.7.0"
+
+  members = [
+    "Motzart",
+  ]
+
+  admins = [
+    "inc4-deployer",
+    "valar999",
+    "ozhadaie",
+    "segovchik",
+    "olehbandrivskyi",
+    "kuroneko-ua",
+  ]
+
+  blocked_users = [
+  ]
+
+}
--- terraform/github/versions.tf
+++ terraform/github/versions.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = ">= 1.3.3"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "inc4"
+
+    workspaces {
+      name = "spaceport-github"
+    }
+  }
 
   required_providers {
     github = {
